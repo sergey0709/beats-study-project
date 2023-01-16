0533e9521f109f891edf273a1f60da09919abe2a
Sergey Sobolev <s56.63@yandex.ru>
2023-01-16 19:03:18 +0300

    first commit

--- scripts/menu-colors.js
+++ scripts/menu-colors.js
@@ -1,10 +1,10 @@
 const measureWidth = item => {
   let reqItemWidth = 0;
   const screenWidth = $(window).width();
-  const container = item.closest(".color__menu"); 
+  const container = item.closest(".color__menu");
   const titlesBlocks = container.find(".colors__title");
   const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
-  
+
   const textContainer = item.find(".colors__container");
   const paddingLeft = parseInt(textContainer.css("padding-left"));
   const paddingRight = parseInt(textContainer.css("padding-right"));
@@ -23,46 +23,15 @@ const measureWidth = item => {
   }
 };
 
-
-
-const closeEveryItemInContainer = container => {
-  const items = container.find(".colors__item");
-  const content = container.find(".colors__content");
-
-  item.removeClass("active");
-  content.width(0);
-}
-
-const openItem = item => {
-  const hiddenContent = item.find(".colors__content");
-  const reqWidth = measureWidth(item);
-  const textBlock = item.find(".colors__container");
-
-  item.addClass("active");
-  hiddenContent.width(reqWidth.container);
-  textBlock.width(reqWidth.textContainer);
-}
-
-$(".colors__title").on("click", e => {
-  e.preventDefault();
-
-  const $this = $(e.currentTarget);
-  const item = $this.closest(".colors__item");
-  const itemOpened = item.hasClass("active");
-  const container = $this.closest(".colors__menu")
-
-  if (itemOpened) {
-    closeEveryItemInContainer();
-  } else {
-    closeEveryItemInContainer();
-    openItem(item);
-  }
-
-  openItem(item);
+$('.colors__item').on('click', function (e) {
+  e.preventDefault()
+  $('.colors__content').css('width', '0px')
+  $(this).next().css('width', '50%')
 })
 
-$(".colors__close").on("click", e => {
-  e.preventDefault();
-
-  closeEveryItemInContainer($('.colors__menu'));
+$('.colors__close-button').on('click', function (e) {
+  e.preventDefault()
+  $('.colors__content').css('width', '0px')
 })
+
+
